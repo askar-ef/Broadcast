df82e33960c23a80ee7c01aa0ccee0f7c4df6573
askar full belajar <108921033+askar-ef@users.noreply.github.com>
2023-11-24 20:57:32 +0700

    feat: photo

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -4,23 +4,23 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import com.example.broadcast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val channelId = "TEST_NOTIF"
-    private val notifId = 4
-
+    private val notifId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             PendingIntent.FLAG_IMMUTABLE
@@ -28,39 +28,44 @@ class MainActivity : AppCompatActivity() {
             0
         }
 
-        val pendingIntent = PendingIntent.getBroadcast(this,0,intent,flag)
+
+        val intent = Intent(this@MainActivity, AplikasiActivity::class.java)
+        intent.putExtra("MESSAGE", "Notification clicked!") // Pesan yang akan ditampilkan saat notifikasi diklik
+        val pendingIntent = (PendingIntent.getBroadcast(this@MainActivity, 0, intent, flag))
 
         val notifManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        with(binding){
-            btnNotif.setOnClickListener{
+        with(binding) {
+            btnNotif.setOnClickListener {
                 val builder = NotificationCompat.Builder(this@MainActivity, channelId)
                     .setSmallIcon(R.drawable.baseline_notifications_24)
+                    .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.askar2))
                     .setContentTitle("Notification Test")
-//                    .setContentText("Hello World")
                     .setAutoCancel(true)
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                     .addAction(0, "baca notip", pendingIntent)
-                    .setStyle(NotificationCompat.BigTextStyle().
-                    bigText("halo brodiii jjsjaksuodwde"))
+                    .setStyle(
+                        NotificationCompat.BigPictureStyle()
+                            .bigPicture(BitmapFactory.decodeResource(resources, R.drawable.askar2))
+                            .bigLargeIcon(null) // Optional, jika ingin mengganti ikon besar
+                            .setSummaryText("halo brodiii jjsjaksuodwde")
+                    )
 
 
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     val notifChannel = NotificationChannel(
-                        channelId, // Id channel
-                        "Notifku", // Nama channel notifikasi
-                        NotificationManager.IMPORTANCE_DEFAULT
+                        channelId,
+                        "Notifku",
+                        NotificationManager.IMPORTANCE_HIGH
                     )
                     with(notifManager) {
                         createNotificationChannel(notifChannel)
                         notify(notifId, builder.build())
                     }
-                }
-                else {
+                } else {
                     notifManager.notify(notifId, builder.build())
                 }
-
             }
         }
     }
